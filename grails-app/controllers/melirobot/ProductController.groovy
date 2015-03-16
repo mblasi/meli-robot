3296d76b77a5ad5ea580de25540ecf88428ad82e
Matías Blasi <matias.blasi@mercadolibre.com>
2015-03-16 12:05:25 -0300

    Controller renamed

--- grails-app/controllers/melirobot/ProductController.groovy
+++ grails-app/controllers/melirobot/ProductController.groovy
@@ -3,7 +3,7 @@ package melirobot
 import grails.converters.JSON
 import org.codehaus.groovy.grails.web.json.JSONArray
 
-class RobotController {
+class ProductController {
 
     def dataService
 
@@ -13,7 +13,6 @@ class RobotController {
 
     def list() {
         def data = JSON.parse(dataService.findAllLocal().toString())
-	System.out << data
 
 	def products = new ArrayList()
 	data.each() {prod -> products << new Product(prod)}
--- test/unit/melirobot/RobotControllerSpec.groovy
+++ test/unit/melirobot/RobotControllerSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(RobotController)
+@TestFor(ProductController)
 class RobotControllerSpec extends Specification {
 
     def setup() {
